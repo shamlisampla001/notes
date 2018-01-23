4ffb880bfeaa2b4b31348cdc7bfedca78c933c9f
shamlisampla001 <easyaccess7600@gmail.com>
2018-01-23 09:31:32 +0530

    new notes

---
+++ cal.py
@@ -0,0 +1,14 @@
+#calculations
+print(3-2)
+print(3+2)
+print(3*2)
+print(3/1)
+height=3
+weight=10
+print(weight*height)
+
+tax = 12.5 / 100
+price = 100.50
+print(price * tax)
+price + _
+round(_,2)
