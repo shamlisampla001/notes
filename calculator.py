42d352d0b825cf37969edee7f93f6958b6816764
amandeep7600 <singh7600aman@gmail.com>
2018-02-02 10:00:10 +0530

    calculater

--- calculator.py
+++ calculator.py
@@ -3,16 +3,21 @@ print("Select 1 For Addition!")
 print("Select 2 For Subtraction!")
 print("Select 3 For Multiplication!")
 print("Select 4 For Division!")
-num=input("Enter Your Choice")
+num=int(input("Enter Your Choice"))
 number1=int(input("Enter the First Number"))
 number2=int(input("Enter the Second Number"))
-switch (num)
-    case 1:
-        number3=number1+number2
-    case 2:
-        number3=number1-number2
-    case 3:
-        number3=number1*number2
-    case 4:
-        number3=number1/number2
+if num==1:
+    number3=number1+number2
+    print(number3)
+elif num==2:
+    number3=number1-number2
+    print(number3)
+elif num==3:
+    number3=number1*number2
+    print(number3)
+elif num==4:
+    number3=number1/number2
+    print(number3)
+else:
+    print("You Enter the Wrong choice")
     
